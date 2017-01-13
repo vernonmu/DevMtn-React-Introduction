dfe94540e91125c964bd714f472ed603697ec8cb
= <webdev.jameslemire@gmail.com>
2017-01-12 19:16:42 -0700

    JL solution - Add button label with state.

--- app/src/App.js
+++ app/src/App.js
@@ -3,16 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-render() {
-	return (
-		<div id='App'>
-			<div id="container-main">
-				<input id="input-task" />
-				<button id="button-submit"> Add </button>
+	constructor(props) {
+		super(props);
+		this.state = {
+			add_label: 'Add'
+		}
+	}
+
+	render() {
+		return (
+			<div id='App'>
+				<div id="container-main">
+					<input id="input-task" />
+					<button id="button-submit"> {this.state.add_label} </button>
+				</div>
 			</div>
-		</div>
-	);
-}
+		);
+	}
 }
 
 export default App;
