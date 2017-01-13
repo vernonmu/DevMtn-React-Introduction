fb979ff99e20e8926955bf8fc68f1339168e6913
= <webdev.jameslemire@gmail.com>
2017-01-12 18:35:15 -0700

    JL solution - :construction:

--- app/src/App.js
+++ app/src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
 render() {
 	return (
 		<div id='App'>
-
+			<div id="container-main">
+				<input id="input-task" />
+				<button id="button-submit"> Add </button>
+			</div>
 		</div>
 	);
 }
