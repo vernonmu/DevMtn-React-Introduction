a789d33ed68dd081e3905451c2b71f2f960160a0
= <webdev.jameslemire@gmail.com>
2017-01-16 15:47:35 -0700

    JL solution - readme.

--- app/src/App.js
+++ app/src/App.js
@@ -1,38 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super();
+		this.state = {
+			display: '0',
+			total: 0,
+			x: 0,
+			y: 0,
+			setDisplay: (num) => {
+				var display;
+				if ( this.state.display.length === 12 ) { return; }
+				display = (this.state.display === '0') ? num : this.state.display + num;
+				this.setState({ display: display });
+			},
+			clearDisplay: () => {
+				this.setState({ display: '0', total: 0, x: 0, y: 0 });
+			},
+			add: () => {
+
+			},
+			subtract: () => {
+
+			},
+			multiply: () => {
+
+			},
+			divide: () => {
+
+			}
+		}
+	}
+
 	render() {
 		return (
 			<div id='App'>
 				<div id="container-main">
-					<img className="remove-highlight" src="./calculator.png" />
-					<div id="calculator-mask">
+					<img className="remove-highlight" src="./calculator.png" alt="calculator" />
+					<div id="calculator-mask" className="remove-highlight">
 
 						<div className="output">
-							<span className="total remove-highlight"></span>
+							<span className="total"> { this.state.display }</span>
 						</div>
 
-						<div className="btn clear remove-highlight"></div>
-						
-						<div className="btn zero remove-highlight"></div>
-						<div className="btn one remove-highlight"></div>
-						<div className="btn two remove-highlight"></div>
-						<div className="btn three remove-highlight"></div>
-						<div className="btn four remove-highlight"></div>
-						<div className="btn five remove-highlight"></div>
-						<div className="btn six remove-highlight"></div>
-						<div className="btn seven remove-highlight"></div>
-						<div className="btn eight remove-highlight"></div>
-						<div className="btn nine remove-highlight"></div>
+						<div className="btn clear" 	onClick={ () => { this.state.clearDisplay(); }}></div>
 
-						<div className="btn decimal remove-highlight"></div>
-						<div className="btn equal remove-highlight"></div>
-						<div className="btn multiply remove-highlight"></div>
-						<div className="btn divide remove-highlight"></div>
-						<div className="btn subtract remove-highlight"></div>
-						<div className="btn add remove-highlight"></div>
+						<div className="btn zero" 	onClick={ () => { this.state.setDisplay('0'); } }></div>
+						<div className="btn one" 	onClick={ () => { this.state.setDisplay('1'); } }></div>
+						<div className="btn two"	onClick={ () => { this.state.setDisplay('2'); } }></div>
+						<div className="btn three"	onClick={ () => { this.state.setDisplay('3'); } }></div>
+						<div className="btn four"	onClick={ () => { this.state.setDisplay('4'); } }></div>
+						<div className="btn five"	onClick={ () => { this.state.setDisplay('5'); } }></div>
+						<div className="btn six"	onClick={ () => { this.state.setDisplay('6'); } }></div>
+						<div className="btn seven"	onClick={ () => { this.state.setDisplay('7'); } }></div>
+						<div className="btn eight"	onClick={ () => { this.state.setDisplay('8'); } }></div>
+						<div className="btn nine"	onClick={ () => { this.state.setDisplay('9'); } }></div>
+
+						<div className="btn decimal"></div>
+						<div className="btn equal"></div>
+						<div className="btn multiply"></div>
+						<div className="btn divide"></div>
+						<div className="btn subtract"></div>
+						<div className="btn add"></div>
 					</div>
 				</div>
 			</div>
